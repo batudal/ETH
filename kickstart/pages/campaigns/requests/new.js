e5dac0c86abe84682c4bfa888df25be691581dd7
Batu Dal <batu.dal@innhof.com>
2018-11-27 01:37:37 +0300

    finalized with errors
    approve / finalize needs dynamic indicators
    page needs to refresh after transaction is done
    some errors when there are multiple campaigns (or rinkeby test network fucked up i dont know)..

--- kickstart/pages/campaigns/requests/new.js
+++ kickstart/pages/campaigns/requests/new.js
@@ -9,7 +9,9 @@ class RequestNew extends Component {
   state ={
     value: '',
     description: '',
-    recipient: ''
+    recipient: '',
+    loading: false,
+    errorMessage: ''
   };
   static async getInitialProps(props) {
     const { address } = props.query;
@@ -19,6 +21,9 @@ class RequestNew extends Component {
     event.preventDefault();
     const campaign = Campaign(this.props.address);
     const {  description, value, recipient } = this.state;
+
+    this.setState({ loading:true, errorMessage:'' });
+
     try{
       const accounts = await web3.eth.getAccounts();
       await campaign.methods.createRequest(
@@ -26,16 +31,23 @@ class RequestNew extends Component {
         web3.utils.toWei(value, 'ether'),
         recipient
       ).send({ from: accounts[0] });
+      Router.pushRoute(`/campaigns/${this.props.address}/requests`);
     } catch(err) {
-
+      this.setState({errorMessage: err.message});
     }
+    this.setState({loading:false});
   };
 
   render(){
     return (
       <Layout>
+      <Link route={`/campaigns/${this.props.address}/requests`}>
+        <a>
+          Back
+        </a>
+      </Link>
       <h3>Create a Request</h3>
-        <Form onSubmit={this.onSubmit}>
+        <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
           <Form.Field>
             <label>Description</label>
             <Input
@@ -57,7 +69,8 @@ class RequestNew extends Component {
               onChange={event => this.setState({ recipient: event.target.value })}
             />
           </Form.Field>
-          <Button primary>Create</Button>
+          <Message error header="Oops!" content={this.state.errorMessage} />
+          <Button primary loading={this.state.loading}>Create</Button>
         </Form>
       </Layout>
     )
